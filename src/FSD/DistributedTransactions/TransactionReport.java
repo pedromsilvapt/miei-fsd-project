f6267f128d87b83f62e82ec13be1b696adda093e
Pedro M. Silva <pemiolsi@hotmail.com>
2018-12-10 10:50:46 +0000

    Fix small typo

--- src/FSD/DistributedTransactions/TransactionReport.java
+++ src/FSD/DistributedTransactions/TransactionReport.java
@@ -9,7 +9,7 @@ public class TransactionReport {
     public TransactionReport ( long id, TransactionState state ) {
         this.id = id;
         this.state = state;
-
+    }
 
     public String toString () {
         return String.format( "TransactionReport( id = %d, state = %s )", this.id, this.state.toString() );
