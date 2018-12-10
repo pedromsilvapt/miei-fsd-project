5288067ad98b6374771632de18c88ea7716e6e17
Pedro M. Silva <pemiolsi@hotmail.com>
2018-12-10 12:55:23 +0000

    Add basic logger functionality

---
+++ src/FSD/Logger.java
@@ -0,0 +1,54 @@
+package FSD;
+
+public class Logger {
+    public enum LogLevel {
+        Debug,
+        Info,
+        Error,
+        Fatal
+    }
+
+    private static LogLevel logLevel = LogLevel.Debug;
+
+    public static LogLevel getLogLevel () {
+        return logLevel;
+    }
+
+    public static void setLogLevel ( LogLevel logLevel ) {
+        Logger.logLevel = logLevel;
+    }
+
+    public static boolean canLog ( LogLevel level ) {
+        return Logger.logLevel.compareTo( level ) <= 0;
+    }
+
+    public static void log ( LogLevel level, String message ) {
+        if ( Logger.canLog( level ) ) {
+            System.out.printf( "[%s] %s\n", level.toString().toUpperCase(), message );
+        }
+    }
+
+    public static void debug ( String message ) {
+        Logger.log( LogLevel.Debug, message );
+    }
+
+    public static void info ( String message ) {
+        Logger.log( LogLevel.Info, message );
+    }
+
+    public static void error ( String message ) {
+        Logger.log( LogLevel.Error, message );
+    }
+
+    public static void error ( Exception exception ) {
+        Logger.error( exception.getMessage() );
+    }
+
+    public static void fatal ( String message ) {
+        Logger.log( LogLevel.Fatal, message );
+    }
+
+    public static void fatal ( Exception exception ) {
+        Logger.fatal( exception.getMessage() );
+    }
+}
